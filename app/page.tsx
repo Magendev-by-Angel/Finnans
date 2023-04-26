02d82c515a457041fdbd2439f57b5ef0f6c70344
William Salas <willbullet52@gmail.com>
2023-04-25 20:18:53 -0500

    test branch 1

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { useRouter, usePathname } from "next/navigation";
 
 const Page = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  console.log(router);
-  console.log(pathname);
-  return <div>Page</div>;
+	//test comment
+	const router = useRouter();
+	const pathname = usePathname();
+	console.log(router);
+	console.log(pathname);
+	return <div>Page</div>;
 };
 export default Page;
