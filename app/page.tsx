6cd8a33579f6498b2ba9fca64d09dbfa3a556c4f
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-04-26 15:26:20 -0500

    created the a test circle percentage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,55 @@
-"use client";
-import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+'use client'
+import React from 'react'
+import { useRouter, usePathname } from 'next/navigation'
+import { useState, useEffect } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 
 const Page = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  console.log(router);
-  console.log(pathname);
-  return <div>Page</div>;
-};
-export default Page;
+	const [percentage, setPercentage] = useState<number>(10)
+	return (
+		<div>
+			<div>
+				<h1>Home</h1>
+				<div className="h-96 w-96">
+					<CircularProgressbar
+						value={percentage}
+						text={`${percentage}%`}
+						styles={buildStyles({
+							// Rotation of path and trail, in number of turns (0-1)
+							rotation: 0,
+
+							// Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+							strokeLinecap: 'round',
+
+							// Text size
+							textSize: '1rem',
+
+							// How long animation takes to go from one percentage to another, in seconds
+							pathTransitionDuration: 0.5,
+
+							// Can specify path transition in more detail, or remove it entirely
+							// pathTransition: 'none',
+
+							// Colors
+							pathColor: `#1b0d2b`,
+							textColor: 'white',
+							trailColor: '#6e1dcc'
+						})}
+					/>
+				</div>
+				<form>
+					<input
+						type="range"
+						min="0"
+						max="100"
+						value={percentage}
+						onChange={e => {
+							setPercentage(parseInt(e.target.value))
+						}}
+					/>
+				</form>
+			</div>
+		</div>
+	)
+}
+export default Page
