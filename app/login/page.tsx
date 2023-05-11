4a0781831da8744c36fa109a4a6d72dc25b902c8
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-05-11 14:00:55 -0500

    added input component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,69 @@
 'use client'
-import React from 'react'
-import { useRouter, usePathname } from 'next/navigation'
+import React, { useState } from 'react'
+// import { useRouter, usePathname } from 'next/navigation'
+
+const Form = () => {
+	const inputStyle = 'w-full rounded-md p-2 text-black'
+	const Input = ({ label, type }) => {
+		return (
+			<>
+				<div className="text-[#535353] mt-4">{label}</div>
+				<input type={type} className={inputStyle} />
+			</>
+		)
+	}
+
+	return (
+		<form
+			action=""
+			className="w-1/2 h-full bg-[#D9D9D9] flex justify-center items-center"
+		>
+			<div className="h-[87%] w-4/5 flex flex-col ">
+				<h1 className="text-black w-full flex justify-center text-4xl">
+					Welcome
+				</h1>
+				<div className="text-[#7B7979] text-[14px] w-full flex leading-4 mt-4">
+					Say goodbye to overspending and hello to financial freedom.
+					Join Finnas and start tracking your expenses with ease
+				</div>
+				<Input type="email" label="email" />
+				<Input type="password" label="password" />
+				<a href="/#" className="text-[#8B72A3] flex justify-end my-4">
+					Forgot password?
+				</a>
+				<button className="bg-[#523869] text-white w-full rounded-md p-2 hover:bg-[#49315e]">
+					Login
+				</button>
+				<span className="w-full flex justify-center my-8 text-[#7B7979] text-sm">
+					or
+				</span>
+				<button className="bg-white text-[#A1A1A1] w-full rounded-md p-2 hover:bg-[#f5f5f5]">
+					Continue with google
+				</button>
+				<div className="flex text-[#535353] font-semibold text-sm w-full justify-center mt-4">
+					You don’t have an account yet?
+					<a href="#" className="ml-1 font-medium text-[#82778D]">
+						Sign up
+					</a>
+				</div>
+				<div className="flex text-[#535353] font-semibold text-sm justify-center mt-4 flex-auto items-end w-full">
+					<div className="w-4/5 text-center">
+						by creating and account you agree to our
+						<a href="#" className="ml-1 font-medium text-[#82778D]">
+							Terms of Service
+						</a>
+					</div>
+				</div>
+			</div>
+		</form>
+	)
+}
 
 const Page = () => {
-	const inputStyle = 'w-full rounded-md p-2 text-black'
 	return (
 		<section className="w-screen h-screen flex justify-center items-center">
-			<div className="w-1/2 h-3/4 flex bg-[#51515133] rounded-2xl overflow-hidden max-w-[864px]">
-				<div className="w-1/2 h-full flex items-center justify-center">
+			<main className="w-1/2 h-3/4 flex bg-[#51515133] rounded-2xl overflow-hidden max-w-[864px]">
+				<aside className="w-1/2 h-full flex items-center justify-center">
 					<div className="h-[87%] w-4/5">
 						<div className="h-3/5 text-[36px] font-semibold">
 							Get in control of your spending with Finnas - the
@@ -40,62 +96,9 @@ const Page = () => {
 							</div>
 						</div>
 					</div>
-				</div>
-				<form
-					action=""
-					className="w-1/2 h-full bg-[#D9D9D9] flex justify-center items-center"
-				>
-					<div className="h-[87%] w-4/5 flex flex-col ">
-						<h1 className="text-black w-full flex justify-center text-4xl">
-							Welcome
-						</h1>
-						<div className="text-[#7B7979] text-[14px] w-full flex leading-4 mt-4">
-							Say goodbye to overspending and hello to financial
-							freedom. Join Finnas and start tracking your
-							expenses with ease
-						</div>
-						<div className="text-[#535353] mt-4">email</div>
-						<input type="email" className={inputStyle} />
-						<div className="text-[#535353] mt-4">password</div>
-						<input type="password" className={inputStyle} />
-						<a
-							href="/#"
-							className="text-[#8B72A3] flex justify-end my-4"
-						>
-							Forgot password?
-						</a>
-						<button className="bg-[#523869] text-white w-full rounded-md p-2">
-							Login
-						</button>
-						<span className="w-full flex justify-center my-8 text-[#7B7979] text-sm">
-							or
-						</span>
-						<button className="bg-white text-[#A1A1A1] w-full rounded-md p-2">
-							Continue with google
-						</button>
-						<div className="flex text-[#535353] font-semibold text-sm w-full justify-center mt-4">
-							You don’t have an account yet?
-							<a
-								href="#"
-								className="ml-1 font-medium text-[#82778D]"
-							>
-								Sign up
-							</a>
-						</div>
-						<div className="flex text-[#535353] font-semibold text-sm justify-center mt-4 flex-auto items-end w-full">
-							<div className="w-4/5 text-center">
-								by creating and account you agree to our
-								<a
-									href="#"
-									className="ml-1 font-medium text-[#82778D]"
-								>
-									Terms of Service
-								</a>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
+				</aside>
+				<Form />
+			</main>
 		</section>
 	)
 }
