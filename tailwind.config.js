321a0c1da70384363f59b251e8982a161486c108
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-05-21 18:56:18 -0500

    Updated colors and added Doughnuts file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}'
-	],
-	theme: {
-		extend: {
-			colors: {
-				table: 'rgba(130, 119, 141, 0.2)',
-				whitegreen: '#70E9AF',
-				whitepurple: '#B176EB',
-				paidgreenbg: '#70E9AF',
-				paidgreenfg: '#148F43',
-				declinedredbg: '#FFEBEB',
-				declinedredfg: '#CC5A5D',
-				pendingyellowbg: '#FEF5E6',
-				pendingyellowfg: '#DFA62E'
-			}
-		}
-	},
-	plugins: []
-}
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        table: "rgba(130, 119, 141, 0.2)",
+        whitegreen: "#70E9AF",
+        whitepurple: "#B176EB",
+        paidgreenbg: "#70E9AF",
+        paidgreenfg: "#148F43",
+        declinedredbg: "#FFEBEB",
+        declinedredfg: "#CC5A5D",
+        pendingyellowbg: "#FEF5E6",
+        pendingyellowfg: "#DFA62E",
+        darkpurple: "#82778D",
+      },
+    },
+  },
+  plugins: [],
+};
