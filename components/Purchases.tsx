6d479a79ba87e4396d67655e22d6defba2faf85d
William Salas <willbullet52@gmail.com>
2023-05-21 22:26:22 -0500

    fixed responsive issue w/ purchase section

--- components/Purchases.tsx
+++ components/Purchases.tsx
@@ -11,9 +11,9 @@ const StatusTag = ({ state }): JSX.Element => {
 	return (
 		<>
 			<div
-				className={`ml-4 text-xs flex justify-center font-bold leading-sm uppercase px-3 py-1 ${statusColor[state]} rounded-md w-1/4`}
+				className={`ml-2 text-xs flex justify-center font-bold leading-sm px-2 py-1 ${statusColor[state]} rounded-md w-1/4`}
 			>
-				{state}
+				{state.charAt(0).toUpperCase() + state.slice(1)}
 			</div>
 		</>
 	)
@@ -21,35 +21,29 @@ const StatusTag = ({ state }): JSX.Element => {
 
 const PurchaseItem = ({ purchases }): JSX.Element => {
 	return (
-		<>
-			<div className="flex flex-col gap-2">
-				{purchases.map(p => {
-					return (
-						<div className="flex flex-row bg-table rounded-lg p-2 w-full items-center">
-							<div className="bg-gray-400 w-8 text-gray-100 aspect-square rounded-md flex justify-center items-center">
-								{/* here we will display the logo with a url, in the meantime we're gonna use a placeholder */}
-								f
-							</div>
-							<p className="pl-2 w-1/4 font-thin text-white">
-								{p.type.charAt(0).toUpperCase() +
-									p.type.slice(1)}
-							</p>
-							<p className="w-1/4 text-white text-ellipsis whitespace-nowrap">
-								{p.date}
-							</p>
-							<p className="w-1/4 flex justify-center text-white font-semibold">
-								{formatter(p.amount)}
-							</p>
+		<div className="flex flex-col gap-2">
+			{purchases.map(p => {
+				return (
+					<div
+						key={p.date}
+						className="flex flex-row bg-table rounded-lg p-2 w-full items-center justify-between"
+					>
+						<div className="bg-gray-400 w-8 h-8 text-gray-100 aspect-square rounded-md mr-2"></div>
+						<p className=" w-1/4 font-thin text-white text-sm whitespace-nowrap max-[1690px]:text-sm max-[1690px]:hidden">
+							{p.type.charAt(0).toUpperCase() + p.type.slice(1)}
+						</p>
+						<p className="w-1/4 text-white text-ellipsis whitespace-nowrap   max-[1690px]:text-sm">
+							{p.date}
+						</p>
+						<p className="w-1/4 flex justify-center text-white font-semibold max-[1690px]:text-sm">
+							{formatter(p.amount)}
+						</p>
 
-							<StatusTag state={p.status} />
-							{/* <p className="w-1/4 flex justify-center">
-								{p.status}
-							</p> */}
-						</div>
-					)
-				})}
-			</div>
-		</>
+						<StatusTag state={p.status} />
+					</div>
+				)
+			})}
+		</div>
 	)
 }
 
